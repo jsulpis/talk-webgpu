f81442acf7581ebc150e55902c2830f0cbcf517c
Julien Sulpis <code@jsulpis.dev>
2026-01-15 14:55:09 +0100

    feat: add data for snowcamp

--- slides/setup/events.ts
+++ slides/setup/events.ts
@@ -6,10 +6,12 @@ export default {
     date: "28/11/2025",
     referencesUrl:
       "https://jsulpis.dev/fr/conferences/evenements/2025-11-28-devfest-lyon-webgpu",
-    qrCodeUrl: import.meta.env.BASE_URL + "qr-code.png",
+    qrCodeUrl: import.meta.env.BASE_URL + "qr-code-devfest-lyon.png",
   },
   snowcamp: {
     name: "SnowCamp",
     date: "15/01/2026",
+    referencesUrl: "https://www.jsulpis.dev/fr/conferences/webgpu",
+    qrCodeUrl: import.meta.env.BASE_URL + "qr-code-snowcamp.png",
   },
 };
